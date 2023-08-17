a285724728cbaa7eb54db17423e962410c9a8b05
SMIT\smit <smitkakadiya2003@gmail.com>
2023-08-17 23:59:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Component/Header';
+import Slider from './Component/Slider';
+import Service from './Component/Service';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+      <Header />
+      <Slider />
+      <Service/>
+    </>
+  );
+}
+
+export default App;
